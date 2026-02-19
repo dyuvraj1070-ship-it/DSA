687829db6c73e1d7e712f2e69c74085b07fe2d4d
Yuvraj Dhiman <dyuvraj1070@gmail.com>
2026-02-19 16:04:49 +0000

    Day 19

---
+++ 100_Days_Of_Code/Day_19.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+    int x = *(const int *)a;
+    int y = *(const int *)b;
+    return (x > y) - (x < y);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    qsort(arr, n, sizeof(int), compare);
+
+    int left = 0, right = n - 1;
+    int min_sum = arr[left] + arr[right];
+    int min_left = left, min_right = right;
+
+    while(left < right) {
+        int sum = arr[left] + arr[right];
+        if(abs(sum) < abs(min_sum)) {
+            min_sum = sum;
+            min_left = left;
+            min_right = right;
+        }
+        if(sum < 0)
+            left++;
+        else
+            right--;
+    }
+
+    printf("%d %d", arr[min_left], arr[min_right]);
+    return 0;
+}
